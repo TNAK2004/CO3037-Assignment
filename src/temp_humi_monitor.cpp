9b530bcf46600306effadf10f85e7ad955225caf
Khoi Tran Nguyen Anh <tnakhoi6@gmail.com>
2025-11-26 16:11:10 +0700

    Update temp_humi_monitor.cpp

--- src/temp_humi_monitor.cpp
+++ src/temp_humi_monitor.cpp
@@ -81,13 +81,13 @@ void temp_humi_monitor(void *pvParameters){
         lcd.setCursor(0, 0);
         lcd.print("Humidity:");
         if (xSemaphoreTake(Sema4LCDhumi, pdMS_TO_TICKS(500))) lcd.print(" Low");
-        else if (xSemaphoreTake(Sema4LCDhumiA, pdMS_TO_TICKS(500))) lcd.print(" Norm");
+        else if (xSemaphoreTake(Sema4LCDhumiA, pdMS_TO_TICKS(500))) lcd.print(" Normal");
         else if (xSemaphoreTake(Sema4LCDhumiB, pdMS_TO_TICKS(500))) lcd.print(" High");
 
         lcd.setCursor(0, 1);
         lcd.print("Temperature:");
         if (xSemaphoreTake(Sema4LCDtemp, pdMS_TO_TICKS(500))) lcd.print(" Cold");
-        else if (xSemaphoreTake(Sema4LCDtempA, pdMS_TO_TICKS(500))) lcd.print(" Norm");
+        else if (xSemaphoreTake(Sema4LCDtempA, pdMS_TO_TICKS(500))) lcd.print(" Normal");
         else if (xSemaphoreTake(Sema4LCDtempB, pdMS_TO_TICKS(500))) lcd.print(" Hot");
 
         // //Update global variables for temperature and humidity
